2472cb30adc1b32df94093fc50ac218e54be9d1d
Young hoon Go <kyhsa93@naver.com>
2020-08-17 15:05:37 +0900

    Update entity

--- entity/user.entity.go
+++ entity/user.entity.go
@@ -1,4 +1,13 @@
 package entity
 
+import "time"
+
 // UserEntity entity for user data
-type UserEntity struct{}
+type UserEntity struct {
+	ID        string     `gorm:"unique; not null; primary_key; index;"`
+	Email     string     `gorm:"unique; not null; index;"`
+	Password  string     `gorm:"not null;"`
+	CreatedAt time.Time  `gorm:"not null;"`
+	UpdatedAt time.Time  `gorm:"not null;"`
+	DeletedAt *time.Time `gorm:"index;"`
+}
