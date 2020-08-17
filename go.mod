3511880720934e439e1e7cefbbd61d65b97eb15a
Young hoon Go <kyhsa93@naver.com>
2020-08-17 20:54:41 +0900

    Add gorm and sava method in repository

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kyhsa93/rest-api-example
 
 go 1.14
 
-require golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+)
--- repository/user.repository.go
+++ repository/user.repository.go
@@ -1,7 +1,24 @@
 package repository
 
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/kyhsa93/rest-api-example/mapper"
+	"github.com/kyhsa93/rest-api-example/model"
+)
+
 // UserRepository user repository
-type UserRepository interface{}
+type UserRepository interface {
+	Save(user model.User)
+}
 
 // UserRepositoryImplement implement for user repository
-type UserRepositoryImplement struct{}
+type UserRepositoryImplement struct {
+	mapper   mapper.UserMapper
+	database *gorm.DB
+}
+
+// Save insert or update given user model data
+func (repository *UserRepositoryImplement) Save(user model.User) {
+	entity := repository.mapper.EntityFromModel(user)
+	repository.database.Save(entity)
+}
