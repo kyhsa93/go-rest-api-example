f631c4176fa2ef02a25ed0d9d744de5780f8b5ed
Young hoon Go <kyhsa93@naver.com>
2020-08-18 21:29:42 +0900

    Add FindByID, FindByEmail into repository

--- repository/user.repository.go
+++ repository/user.repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/kyhsa93/rest-api-example/entity"
 	"github.com/kyhsa93/rest-api-example/mapper"
 	"github.com/kyhsa93/rest-api-example/model"
 )
@@ -9,6 +10,8 @@ import (
 // UserRepository user repository
 type UserRepository interface {
 	Save(user model.User)
+	FindByID(userID string) model.User
+	FindByEmail(email string) model.User
 }
 
 // UserRepositoryImplement implement for user repository
@@ -21,4 +24,18 @@ type UserRepositoryImplement struct {
 func (repository *UserRepositoryImplement) Save(user model.User) {
 	entity := repository.mapper.EntityFromModel(user)
 	repository.database.Save(entity)
+}
+
+// FindByID find single user data by ID
+func (repository *UserRepositoryImplement) FindByID(userID string) model.User {
+	user := entity.UserEntity{}
+	repository.database.First(&user, userID)
+	return repository.mapper.ModelFromEntity(user)
+}
+
+// FindByEmail find single user data by email
+func (repository *UserRepositoryImplement) FindByEmail(email string) model.User {
+	user := entity.UserEntity{}
+	repository.database.Where(&entity.UserEntity{Email: email}).First(&user)
+	return repository.mapper.ModelFromEntity(user)
 }
