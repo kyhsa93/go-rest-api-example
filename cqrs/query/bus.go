b6cad758db55fd4b0ee97bacd1ee4b1e74da4044
Young hoon Go <kyhsa93@naver.com>
2020-08-19 23:44:41 +0900

    Update CQRS Query Bus

--- cqrs/query/bus.go
+++ cqrs/query/bus.go
@@ -1,7 +1,27 @@
 package query
 
+import (
+	"errors"
+
+	"github.com/kyhsa93/rest-api-example/query"
+)
+
 // Bus handle query
-type Bus interface{}
+type Bus interface {
+	Handle(query interface{}) (interface{}, error)
+}
 
 // BusImplement query bu implement
-type BusImplement struct{}
+type BusImplement struct {
+	findUserQueryHandler query.FindUserQueryHandler
+}
+
+// Handle call and excute query handlers by query type
+func (bus *BusImplement) Handle(userQuery interface{}) (interface{}, error) {
+	switch userQuery := userQuery.(type) {
+	case *query.FindUserQuery:
+		return bus.findUserQueryHandler.Handle(userQuery)
+	default:
+		return nil, errors.New("can not handle query")
+	}
+}
--- cqrs/query/bus_test.go
+++ cqrs/query/bus_test.go
@@ -1,1 +1,26 @@
 package query
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/kyhsa93/rest-api-example/model"
+	"github.com/kyhsa93/rest-api-example/query"
+)
+
+type findUserQueryHandlerMock struct{}
+
+func (handler *findUserQueryHandlerMock) Handle(query *query.FindUserQuery) (model.User, error) {
+	return nil, errors.New("findUserQueryHandler")
+}
+
+func TestHandle(t *testing.T) {
+	bus := BusImplement{
+		findUserQueryHandler: &findUserQueryHandlerMock{},
+	}
+
+	findUserQuery := query.FindUserQuery{}
+	if _, err := bus.Handle(&findUserQuery); err.Error() != "findUserQueryHandler" {
+		t.Fatal("queryBus can not call find user query handler")
+	}
+}
