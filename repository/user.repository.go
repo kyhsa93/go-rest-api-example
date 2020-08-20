b60c9cbac4c1e47ea8fbcc8dcaee11e62007448d
Young hoon Go <kyhsa93@naver.com>
2020-08-20 13:36:50 +0900

    Remove mapper and add convert method in repository

--- repository/user.repository.go
+++ repository/user.repository.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/kyhsa93/rest-api-example/entity"
-	"github.com/kyhsa93/rest-api-example/mapper"
 	"github.com/kyhsa93/rest-api-example/model"
 )
 
@@ -17,13 +16,12 @@ type UserRepository interface {
 
 // UserRepositoryImplement implement for user repository
 type UserRepositoryImplement struct {
-	mapper   mapper.UserMapper
 	database *gorm.DB
 }
 
 // Save insert or update given user model data
 func (repository *UserRepositoryImplement) Save(user model.User) {
-	entity := repository.mapper.EntityFromModel(user)
+	entity := entityFromModel(user)
 	repository.database.Save(entity)
 }
 
@@ -31,18 +29,38 @@ func (repository *UserRepositoryImplement) Save(user model.User) {
 func (repository *UserRepositoryImplement) FindByID(userID string) model.User {
 	user := entity.UserEntity{}
 	repository.database.First(&user, userID)
-	return repository.mapper.ModelFromEntity(user)
+	return modelFromEntity(user)
 }
 
 // FindByEmail find single user data by email
 func (repository *UserRepositoryImplement) FindByEmail(email string) model.User {
 	user := entity.UserEntity{}
 	repository.database.Where(&entity.UserEntity{Email: email}).First(&user)
-	return repository.mapper.ModelFromEntity(user)
+	return modelFromEntity(user)
 }
 
 // Delete delete user data
 func (repository *UserRepositoryImplement) Delete(user model.User) {
-	entity := repository.mapper.EntityFromModel(user)
+	entity := entityFromModel(user)
 	repository.database.Delete(entity)
+}
+
+func entityFromModel(model model.User) *entity.UserEntity {
+	return &entity.UserEntity{
+		ID:        model.GetID(),
+		Email:     model.GetEmail(),
+		Password:  model.GetPassword(),
+		CreatedAt: model.GetCreatedAt(),
+		UpdatedAt: model.GetUpdatedAt(),
+	}
+}
+
+func modelFromEntity(entity entity.UserEntity) model.User {
+	user := &model.UserImplement{}
+	user.SetID(entity.ID)
+	user.SetEmail(entity.Email)
+	user.SetPassword(entity.Password)
+	user.SetCreatedAt(entity.CreatedAt)
+	user.SetUpdatedAt(entity.UpdatedAt)
+	return user
 }
